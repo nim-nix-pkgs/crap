b5e2add5db7a51a9471d93b96ae9d1488f90234f
hugosenari <hugosenari@gmail.com>
2022-03-12 04:14:19 -0300

    chore: re index crap

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''`rm` files without fear'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-crap-master.flake = false;
+  inputs.src-crap-master.owner = "icyphox";
+  inputs.src-crap-master.ref   = "refs/heads/master";
+  inputs.src-crap-master.repo  = "crap";
+  inputs.src-crap-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-crap-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-crap-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ v0_2_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''`rm` files without fear'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-crap-v0_2_0.flake = false;
+  inputs.src-crap-v0_2_0.owner = "icyphox";
+  inputs.src-crap-v0_2_0.ref   = "refs/tags/v0.2.0";
+  inputs.src-crap-v0_2_0.repo  = "crap";
+  inputs.src-crap-v0_2_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-crap-v0_2_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-crap-v0_2_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
