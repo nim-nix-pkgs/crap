ca9c844d49670408da07565ed5db29daeb7c0e10
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:52 -0300

    chore: re-index ../nimpkgs/c/crap/v0_2_1

--- v0_2_1/flake.nix
+++ v0_2_1/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-crap-v0_2_1.flake = false;
   inputs.src-crap-v0_2_1.owner = "icyphox";
-  inputs.src-crap-v0_2_1.ref   = "refs/tags/v0.2.1";
+  inputs.src-crap-v0_2_1.ref   = "v0_2_1";
   inputs.src-crap-v0_2_1.repo  = "crap";
   inputs.src-crap-v0_2_1.type  = "github";
   
